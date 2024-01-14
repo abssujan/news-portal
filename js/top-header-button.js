4c1a6f624961d85a5860b10d338897581e2e0a36
abssujan <abdussabursujan@gmail.com>
2024-01-14 19:27:14 +0600

    complete top header

--- js/top-header-button.js
+++ js/top-header-button.js
@@ -1,21 +1,21 @@
-const buttons = document.querySelectorAll('.top-header-button');
+// const buttons = document.querySelectorAll('.top-header-button');
 
-let activeButton = null;
+// let activeButton = null;
 
-buttons.forEach(function(button) {
-  button.addEventListener('click', function() {
-    // Remove the class from the currently active button
-    if (activeButton) {
-      activeButton.classList.remove('hover:text-indigo-600', 'hover:border-b-2', 'hover:border-indigo-600', 'active-button');
-    }
+// buttons.forEach(function(button) {
+//   button.addEventListener('click', function() {
+//     // Remove the class from the currently active button
+//     if (activeButton) {
+//       activeButton.classList.remove('hover:text-indigo-600', 'hover:border-b-2', 'hover:border-indigo-600', 'active-button');
+//     }
 
-    // Add the class to the clicked button
-    button.classList.add('hover:text-indigo-600', 'hover:border-b-2', 'hover:border-indigo-600', 'active-button');
+//     // Add the class to the clicked button
+//     button.classList.add('hover:text-indigo-600', 'hover:border-b-2', 'hover:border-indigo-600', 'active-button');
 
-    // Set the clicked button as the currently active button
-    activeButton = button;
+//     // Set the clicked button as the currently active button
+//     activeButton = button;
 
-    // Your click event logic here
-    console.log('Button clicked!');
-  });
-});
+//     // Your click event logic here
+//     console.log('Button clicked!');
+//   });
+// });
